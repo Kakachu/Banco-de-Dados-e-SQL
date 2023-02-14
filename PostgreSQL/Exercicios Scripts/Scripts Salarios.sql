e187f05c3e65a0001dbfae7e77c3a8757bc5bb73
kakachu <kauajardim2004@hotmail.com>
2023-02-14 17:01:09 -0300

    Machine Learning Variavel Dummy

---
+++ PostgreSQL/Exercicios Scripts/Scripts Salarios.sql
@@ -0,0 +1,13 @@
+/*EXERCICIO SALARIOS*/
+
+/* Qual a moda dos salarios. A moda dos salários diz algo de relevante? */
+
+/* Qual a moda departamental = Qual o departamento que mais emprega? */
+
+/* Qual o desvio padrao de cada departamento? */
+
+/* Calcule a mediana salarial de todo o set de dados */
+
+/* Qual a amplitude de todos os salarios */
+
+/* Calcule as principas medidas estatísticas por departamento */
